7cafd84cd2509d5f45d7f1414018b3bfea8dcf8a
Liz Burton <liz.burton147@gmail.com>
2017-11-24 22:06:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,5 @@
 require 'pry'
-def lizs_sort_array(sort_array, order)
+def lizs_sort_array(sort_array, order="asc")
   sorted = false
   sort_array.each_with_index.map do | a, index |
     while sorted == false
@@ -19,6 +19,8 @@ def switch?(a,b,order)
   (order == "asc" && a > b) || (order == "desc" && a < b)
 end
 
+lizs_sort_array([17, 3, 12, 1, 2])
+
  def sort_array_asc(unsorted_array)
    unsorted_array.sort { | a, b | a <=> b  }
  end
@@ -26,5 +28,3 @@ end
 def sort_array_desc(unsorted_array)
   unsorted_array.sort { | b, a | a <=> b  }
 end
-
-sort_array_asc([17, 3, 12, 1, 2])
