58df06e1e54ed84ca83c7391c9fbcd6ab7d882a6
Liz Burton <liz.burton147@gmail.com>
2017-11-24 22:05:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,25 +1,23 @@
 require 'pry'
-def sort_array_asc(unsorted_array)
-#   unsorted_array.sort { | a, b | a <=> b  }
-  sorted_array = []
-  unsorted_array.each_with_index.map do | a, index |
-    temp_array = []
+def lizs_sort_array(sort_array, order)
+  sorted = false
+  sort_array.each_with_index.map do | a, index |
+    while sorted == false
     if index < unsorted_array.length - 1
-
+      sorted = true
       if unsorted_array[index + 1] < a
-        temp_array << unsorted_array[index + 1]
-        temp_array << a
-      else
-        temp_array << a
-        temp_array << unsorted_array[index + 1]
-        #binding.pry
+        if switch?(a, sort_array[index + 1], order)
+          sorted=false
+          unsorted_array[i], unsorted_array[i + 1] = unsorted_array[i + 1], unsorted_array[i]
+        end
       end
     end
-    sorted_array << temp_array[0]
-    sorted_array << temp_array[1]
-
   end
- end
+end
+
+def switch?(a,b,order)
+  (order == "asc" && a > b) || (order == "desc" && a < b)
+end
 
  def sort_array_asc(unsorted_array)
    unsorted_array.sort { | a, b | a <=> b  }
