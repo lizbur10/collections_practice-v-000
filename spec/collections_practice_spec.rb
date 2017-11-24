9de85823ef49cdd9203ab620e9abb8b8390b97a5
Liz Burton <liz.burton147@gmail.com>
2017-11-24 23:49:58 +0000

    automatically backed up by learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -64,7 +64,7 @@ describe 'collections practice' do
     #for a bonus, look up the `.inject` method to get this test passing
     it 'uses inject' do
       numbers = [1,2,3]
-      expect(sum_array(numbers)).to receive(:inject)
+      expect(numbers).to receive(:inject)
     end
     it 'sum all the numbers in the following array' do
       expect(sum_array([11,4,7,8,9,100,134])).to eq(273)
